9cf98c886d47076565e1708b85d5869171259bb5
Jordan Taylor <jordan.a.taylor@gmail.com>
2024-08-13 15:22:32 -0400

    Created division.py to test github

---
+++ division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def num(n):
+    try:
+        return int(n)
+    except ValueError:
+        return float(n)
+
+n = num(input("Enter numerator: "))
+d = num(input("Enter denoinator: "))
+print(n / d)
